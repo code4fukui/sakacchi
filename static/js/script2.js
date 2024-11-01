2ef22bf4f51718e8f69a2ab8bf33573c762bbfda
edtechTR <tr_ke_ro405@yahoo.co.jp>
2024-11-01 23:06:35 +0900

    Add files via upload

--- static/js/script2.js
+++ static/js/script2.js
@@ -113,7 +113,7 @@ export function handleCardAcquire() {
     localStorage.setItem('cards', JSON.stringify(currentCards));
     renderCards();
     showNotification(`新しいカード「${newCard.name}」を獲得しました！`);
-    location.href = "folder.html"
+    location.href = "https://ai-sakai.sabae.cc/folder.html"
 }
 
 //ローカルストレージに保存されている「カード」データを初期化し、画面にその初期状態のデータを再表示させる処理
