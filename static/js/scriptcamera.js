1aff77b005c1c563d5d6dab59d575214a474faa6
ponpongan <t.taira.1892@gmail.com>
2025-01-08 12:04:46 +0900

    Update scriptcamera.js

--- static/js/scriptcamera.js
+++ static/js/scriptcamera.js
@@ -44,9 +44,10 @@ captureButton.addEventListener('click', async () => {
     // 結果の処理
     if (res && typeof res.is_waste === 'boolean' && res.description) {
       resultElement.textContent = res.description;
-      sleep(30000);
+      
       if (res.is_waste) {
         localStorage.setItem('cardAcquired', 'false');//カードが獲得できるように解除
+         sleep(30000);
         location.href = "getcard.html";  //ゴミと判定された場合の処理
       } else {
         alert("これはごみではありません");
